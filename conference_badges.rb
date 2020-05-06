eb9af87fb9e92c109c1d4f5e15e06e4f7fbdf7a8
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-06 23:48:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,11 +6,20 @@ def batch_badge_creator(names)
   names.collect do |names|
     badge_maker(name)
 end
+end
 
-def room_assignments(names)
-  room = 0
-  speakers.collect do |names|
-    room +=1
-    "Hello #{name}! You'll be assigned to room #{room}!"
-     
+  def room_assignments(names)
+    room = 0
+    names.collect do |names|
+      room +=1
+      "Hello #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(names)
+  badge_batch_creator(names).each do |value|
+    puts value
+  end
+  assign_rooms(names).each do |value|
+  end
 end
